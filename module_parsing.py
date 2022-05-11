37c697aa908d45ab7cba24c2eebe360298004570
Сычиков <100536461+1ncucubu5@users.noreply.github.com>
2022-05-11 19:04:02 +0300

    Update module_parsing.py

--- module_parsing.py
+++ module_parsing.py
@@ -1,18 +1,38 @@
 import colorama
 from colorama import Fore, Back, Style
+from module_downloading import download
+from module_downloading import make_main_folder
 
-def get_links(FileWay):
-    try:
-        with open(FileWay, "r") as File:
-            ArrayOfLinks = []
-            for link in File:
+main_folder = "111"
+
+def get_links(FileWay, iteration_level):
+    global main_folder
+    #try:
+    with open(FileWay, "r") as File:
+        ArrayOfLinks = []
+        count = 0
+        for link in File:
+            if check_for_download(link) == 0:
                 link = link[:link.find('/n')]
                 ArrayOfLinks.append(link)
+            else:
+                if main_folder == "111":
+                    main_folder = make_main_folder()
+                link = download(link, iteration_level, count, main_folder)
+                ArrayOfLinks.append(link)
+            count = count + 1
 
-            return ArrayOfLinks
+        return ArrayOfLinks
 
-    except:
-        print("File reading error")
+    #except:
+        #print("Links reading error")
+
+def check_for_download(link):
+    part_of_link = link[:link.find(':')]
+    if part_of_link == "file":
+        return 0
+    else:
+        return 1
 
 def white(strr):
     return Fore.WHITE + " " + str(strr) + " " + Fore.RED
